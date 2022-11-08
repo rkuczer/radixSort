b2c0b089da580b26dc0060aba91895e6213e27d1
rkuczer <57370393+rkuczer@users.noreply.github.com>
2022-11-08 16:11:27 -0500

    new stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,33 @@
 package com.company;
 import java.util.*;
 
+//******* Missing ********* - tried to get array to not print out last comma at end of list in the printsortedarray function
+
 class Main {
-    static int max(int queue[],int length)
+
+    public static void main(String[] args) //main function actual starts here
     {
-        int maxvalue=queue[0];
-        for (int i=1;i<length;i++)
-            if (queue[i]>maxvalue)
+        //function path sortArray --> max --> countSort --> printSortedArray
+
+        int[] arrayToSort ={1700,78,21,190,1802,24,200,6600,800,9000};
+
+        int length=arrayToSort.length; //sets the length variable to the length of arrayToSort
+
+        sortArray(arrayToSort,length); //calls sortmain
+
+        printSortedArray(arrayToSort,length);//calls printSortedArray method
+    }
+
+    static int max(int queue[],int length) //static method that passes int array and int to find max num in initial array
+    {
+        int maxvalue=queue[0];//sets the first value to "max"
+
+        for (int i=1;i<length;i++) //for loop that starts at 1 and iterates for the length of the list
+
+            if (queue[i]>maxvalue) //sets queue value to max if it is greater than the previous max value
                 maxvalue=queue[i];
-        return maxvalue;
+
+        return maxvalue; //returns final max
     }
 
     static void countSort(int queue[],int length,int k) //this method is used for doing counting sort
@@ -19,31 +38,32 @@ class Main {
 
         Arrays.fill(temp,0); //sets all indexes of array to 0
 
-        for (i=0;i<length;i++) // fills temp array with
+        for (i=0;i<length;i++) // fills temp array with the remainder
             temp[(queue[i]/k)%10]++; //mod 10
 
         for (i=1;i<10;i++)
             temp[i]+=temp[i-1]; //decreases value by 1 for alternative distribution data structure
 
-        for (i=length-1;i>=0;i--) // sort array
+        for (i=length-1;i>=0;i--) // sort array, value starts at n-1 element, stops when i >=0 and decrements
         {
             sortedarray[temp[(queue[i]/k)%10]-1]=queue[i]; //whatever result of s/k mod 10 - 1 in your array
-            temp[(queue[i]/k)%10]--; //decreases temp value in alt array
+            temp[(queue[i]/k)%10]--; //finds the remainder with the element in queue over k
         }
-        for (i=0;i<length;i++) //transfers data from array to array
+        for (i=0;i<length;i++) //transfers data from beginning queue array to final sorted array
             queue[i]=sortedarray[i];
     }
 
-    static void sortArray(int array[],int length) // this method is main function
+    static void sortArray(int array[],int length) // this method is the main function
     {
-        int max = max(array, length); //calling find max method
-        for (int k=1;max/k >0;k*=10)
-            countSort(array, length,k);
+        int max = max(array, length); //calling find max method with array and length as inputs from main function
+        for (int k=1;max/k >0;k*=10) //calls count sort with k starting at 1 going till max/k is greater than 0
+            //here *= multiplies a variable by the value of 10 and assigns the result to the variable
+            countSort(array, length,k); //call the count sort method on the sorted array
     }
 
-    static void printSortedArray(int array[],int length)
+    static void printSortedArray(int array[],int length) //function that prints the sorted array
     {
-        for (int i=0;i<length;i++)
+        for (int i=0;i<length;i++) //i starts at 0, goes till i is < length of array
             if(length==i) {
                 System.out.print(array[i]);
             }
@@ -52,13 +72,6 @@ class Main {
             }
     }
 
-    public static void main(String[] args) //main function actual starts here
-    {
-        //function path sortArray --> max --> countSort --> printSortedArray
-        int[] arrayToSort ={1700,78,21,190,1802,24,200,6600,800,9000};
-        int length=arrayToSort.length;
-        sortArray(arrayToSort,length); //calls sortmain
-        printSortedArray(arrayToSort,length);//calls printSortedArray method
-    }
+
 
 }
