b4af0deb245588410b3c74bbdf201235e3376933
rkuczer <57370393+rkuczer@users.noreply.github.com>
2022-11-11 17:57:21 -0500

    new stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,17 @@
 package com.company;
 import java.util.*;
 
-//******* Missing ********* - tried to get array to not print out last comma at end of list in the printsortedarray function
-
 class Main {
 
     public static void main(String[] args) //main function actual program starts here
     {
         //function path sortArray --> max --> countSort --> printSortedArray
 
-        int[] arrayToSort ={10,20,30, 200, 100, 24, 666, 6600, 800, 9111};
+        int[] arrayToSort ={9111,20,30,200,100,24,666,6600,800,10}; //
 
         int length=arrayToSort.length; //sets the length variable to the length of arrayToSort
 
-        organizeArray(arrayToSort,length); //calls sortmain
+        organizeArray(arrayToSort,length); //calls sortmain with the arraytoSort premade array with corresponding length
 
         printSortedArray(arrayToSort,length);//calls printSortedArray method
     }
@@ -30,18 +28,25 @@ class Main {
         return maxvalue; //returns final max
     }
 
+    static void organizeArray(int array[],int length) // this method is the main function
+    {
+        int max = max(array, length); //calling find max method with array and length as inputs from main function
+        for (int k=1;max/k >0;k*=10) //calls count sort with k starting at 1 going till max/k is greater than 0
+            //here *= multiplies a variable by the value of 10 and assigns the result to the variable
+            countSort(array, length,k); //call the count sort method on the sorted array
+    }
+
     static void countSort(int queue[],int length,int k) //this method is used for doing counting sort
     {
         int sortedarray[] = new int[length]; //creates an alternative array with length as the index
         int i;
-        int temp[] = new int[10]; //sets a temporary value for the seconday data structure for count sort
-
+        int temp[] = new int[10]; //sets a temporary value for the secondary data structure for count sort
         Arrays.fill(temp,0); //sets all indexes of array to 0
 
         for (i=0;i<length;i++) // fills temp array with the remainder
-            temp[(queue[i]/k)%10]++; //mod 10
+            temp[(queue[i]/k)%10]++; //mod 10 over k
 
-        for (i=1;i<10;i++)
+        for (i=1;i<10;i++)// I is 10 since we know array size to be 10
             temp[i]+=temp[i-1]; //decreases value by 1 for alternative distribution data structure
 
         for (i=length-1;i>=0;i--) // sort array, value starts at n-1 element, stops when i >=0 and decrements
@@ -53,20 +58,12 @@ class Main {
             queue[i]=sortedarray[i];
     }
 
-    static void organizeArray(int array[],int length) // this method is the main function
-    {
-        int max = max(array, length); //calling find max method with array and length as inputs from main function
-        for (int k=1;max/k >0;k*=10) //calls count sort with k starting at 1 going till max/k is greater than 0
-            //here *= multiplies a variable by the value of 10 and assigns the result to the variable
-            countSort(array, length,k); //call the count sort method on the sorted array
-    }
-
     static void printSortedArray(int array[],int length) //function that prints the sorted array
     {
         for (int i=0;i<10;i++) //i starts at 0, goes till i is < length of array
 
-            if(i==length) {
-                System.out.print(array[i]);
+            if(i==9) { //since we know the array is going to be of size 10, i is being set to 9 since it is the end of the list. Can change 9 to "length" if it is of an unknown size input
+                System.out.print(array[i] + ".");
             }
             else {
                 System.out.print(array[i] + ",");
